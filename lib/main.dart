6104af6765531b1a1aa4dd4af58faf91b6885cf7
aliakberaakash <ali852609@gmail.com>
2020-10-31 13:35:37 +0600

    Added theme data

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         accentColor: accent,
 
         // Define the default font family.
-        fontFamily: 'Georgia',
+        fontFamily: 'Quicksand',
 
       ),
       home: HomePage(),
--- lib/utils/my_colors.dart
+++ lib/utils/my_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/rendering.dart';
 
 const white = Color(0xffffffff);
 const black = Color(0xff000000);
-const primary = Color(0xffffe2e2);
-const primary_dark = Color(0xffffc7c7);
-const accent = Color(0xffaaaaaa);
+const matte_black = Color(0xff212121);
+const primary = Color(0xff3C4053);
+const primary_dark = Color(0xff34384A);
+const accent = Color(0xffFFBBB4);
